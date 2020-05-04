b0183ffdc207a901a3b44149770a37800e463535
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2020-05-04 22:01:54 +0200

    Trying to solve conflict in akka-http

--- core/project/Dependencies.scala
+++ core/project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 // format: OFF
 object Version {
   val Akka         = "2.5.29"
-  val AkkaHttp     = "10.1.9"
-  val AkkaMgmt     = "1.0.3"
+  val AkkaHttp     = "10.1.10"
+  val AkkaMgmt     = "1.0.6"
   val AlpakkaKafka = "2.0.2"
   val Scala        = "2.12.9"
   val Spark        = "2.4.5"
@@ -14,19 +14,20 @@ object Version {
 
 object Library {
   // External Libraries
-  val AkkaHttp              = "com.typesafe.akka"     %% "akka-http"                 % Version.AkkaHttp
-  val AkkaHttpJackson       = "com.typesafe.akka"     %% "akka-http-jackson"         % Version.AkkaHttp
-  val AkkaHttpSprayJson     = "com.typesafe.akka"     %% "akka-http-spray-json"      % Version.AkkaHttp
-  val AkkaStream            = "com.typesafe.akka"     %% "akka-stream"               % Version.Akka
-  val AkkaSlf4j             = "com.typesafe.akka"     %% "akka-slf4j"                % Version.Akka
-  val AkkaStreamContrib     = "com.typesafe.akka"     %% "akka-stream-contrib"       % "0.10"
-  val AkkaStreamKafka       = "com.typesafe.akka"     %% "akka-stream-kafka"         % Version.AlpakkaKafka exclude("com.fasterxml.jackson.core","jackson-databind") exclude("com.fasterxml.jackson.module", "jackson-module-scala")
-  val AkkaStreamKafkaTestkit = "com.typesafe.akka"    %% "akka-stream-kafka-testkit" % Version.AlpakkaKafka exclude("com.typesafe.akka", "akka-stream-testkit")
-  val AkkaCluster           = "com.typesafe.akka"     %% "akka-cluster"              % Version.Akka
-  val AkkaManagement        = "com.lightbend.akka.management" %% "akka-management"   % Version.AkkaMgmt
+  val AkkaHttp               = "com.typesafe.akka"             %% "akka-http"                         % Version.AkkaHttp
+  val AkkaHttpJackson        = "com.typesafe.akka"             %% "akka-http-jackson"                 % Version.AkkaHttp
+  val AkkaHttpSprayJson      = "com.typesafe.akka"             %% "akka-http-spray-json"              % Version.AkkaHttp
+  val AkkaStream             = "com.typesafe.akka"             %% "akka-stream"                       % Version.Akka
+  val AkkaSlf4j              = "com.typesafe.akka"             %% "akka-slf4j"                        % Version.Akka
+  val AkkaStreamContrib      = "com.typesafe.akka"             %% "akka-stream-contrib"               % "0.10"
+  val AkkaStreamKafka        = "com.typesafe.akka"             %% "akka-stream-kafka"                 % Version.AlpakkaKafka exclude("com.fasterxml.jackson.core","jackson-databind") exclude("com.fasterxml.jackson.module", "jackson-module-scala")
+  val AkkaStreamKafkaTestkit = "com.typesafe.akka"             %% "akka-stream-kafka-testkit"         % Version.AlpakkaKafka exclude("com.typesafe.akka", "akka-stream-testkit")
+  val AkkaCluster            = "com.typesafe.akka"             %% "akka-cluster"                      % Version.Akka
+  val AkkaManagement         = "com.lightbend.akka.management" %% "akka-management"                   % Version.AkkaMgmt
   val AkkaClusterBootstrap   = "com.lightbend.akka.management" %% "akka-management-cluster-bootstrap" % Version.AkkaMgmt
-  val AkkaDiscovery         = "com.typesafe.akka"     %% "akka-discovery"            % Version.Akka
-  val AkkaDiscoveryK8       = "com.lightbend.akka.discovery" %% "akka-discovery-kubernetes-api" % Version.AkkaMgmt
+  val AkkaDiscovery          = "com.typesafe.akka"             %% "akka-discovery"                    % Version.Akka
+  val AkkaDiscoveryK8        = "com.lightbend.akka.discovery"  %% "akka-discovery-kubernetes-api"     % Version.AkkaMgmt
+
   val EmbeddedKafkaOrg      = "io.github.embeddedkafka"
   val EmbeddedKafka         = EmbeddedKafkaOrg        %% "embedded-kafka"           % Version.Kafka exclude("org.apache.avro", "avro") exclude("com.fasterxml.jackson.core","jackson-databind") exclude("com.fasterxml.jackson.module", "jackson-module-scala")
   val Ficus                 = "com.iheart"            %% "ficus"                    % "1.4.7"
@@ -39,16 +40,16 @@ object Library {
 
   val JacksonScalaModule    = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.10.0"
   val JacksonDatabind       = "com.fasterxml.jackson.core"    % "jackson-databind"     % "2.9.9.1"
-  val SparkJacksonDatabind  = "com.fasterxml.jackson.core"    % "jackson-databind"     % "2.6.7.3"
 
   val Skuber                = "io.skuber"             %% "skuber"                   % "2.4.0" exclude("com.fasterxml.jackson.core","jackson-databind")
 
-  val Spark                 = "org.apache.spark"      %% "spark-core"               % Version.Spark
-  val SparkMllib            = "org.apache.spark"      %% "spark-mllib"              % Version.Spark
-  val SparkSql              = "org.apache.spark"      %% "spark-sql"                % Version.Spark
-  val SparkSqlKafka         = "org.apache.spark"      %% "spark-sql-kafka-0-10"     % Version.Spark
-  val SparkStreaming        = "org.apache.spark"      %% "spark-streaming"          % Version.Spark
-  val ScalaTestUnscoped     = "org.scalatest"         %% "scalatest"                % "3.0.8"
+  val Spark                 = "org.apache.spark"           %% "spark-core"           % Version.Spark
+  val SparkJacksonDatabind  = "com.fasterxml.jackson.core"  % "jackson-databind"     % "2.6.7.3"
+  val SparkMllib            = "org.apache.spark"           %% "spark-mllib"          % Version.Spark
+  val SparkSql              = "org.apache.spark"           %% "spark-sql"            % Version.Spark
+  val SparkSqlKafka         = "org.apache.spark"           %% "spark-sql-kafka-0-10" % Version.Spark
+  val SparkStreaming        = "org.apache.spark"           %% "spark-streaming"      % Version.Spark
+  val ScalaTestUnscoped     = "org.scalatest"              %% "scalatest"            % "3.0.8"
 
   val Flink                 = "org.apache.flink"      %% "flink-scala"              % Version.Flink
   val FlinkStreaming        = "org.apache.flink"      %% "flink-streaming-scala"    % Version.Flink
